c82e13c0229a7bd9f0cb51b65a89784c38127951
Bianca Tudora <bianca.tudora@gmail.com>
2017-10-12 12:39:56 +0300

    first commit

---
+++ AndroidTestRaiffeisen/app/src/main/java/com/adonissoft/androidtestraiffeisen/api/ApiServiceFactory.java
@@ -0,0 +1,43 @@
+package com.adonissoft.androidtestraiffeisen.api;
+
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import retrofit.RestAdapter;
+import retrofit.client.OkClient;
+
+/**
+ * Created by Bianca on 12.10.2017.
+ */
+
+public class ApiServiceFactory {
+
+    private static final String BASE_URL = "https://randomuser.me/api/";
+
+
+    private ApiServiceFactory() {
+    }
+
+
+    public static RaiffeisenApiService createRaiffeisenApiService() {
+        RestAdapter adapter = createRestAdapter(BASE_URL);
+        return adapter.create(RaiffeisenApiService.class);
+    }
+
+    private static RestAdapter createRestAdapter(String baseURL) {
+        RestAdapter.Builder builder = new RestAdapter.Builder()
+                .setEndpoint(baseURL)
+                .setClient(new OkClient(new OkHttpClient()))
+                .setLog(new RestAdapter.Log() {
+                    @Override
+                    public void log(String message) {
+                        Log.d("Retrofit", message);
+                    }
+                });
+        RestAdapter adapter = builder.build();
+        adapter.setLogLevel(RestAdapter.LogLevel.FULL);
+        return adapter;
+    }
+
+}
---
+++ AndroidTestRaiffeisen/app/src/main/java/com/adonissoft/androidtestraiffeisen/api/RaiffeisenApiService.java
@@ -0,0 +1,10 @@
+package com.adonissoft.androidtestraiffeisen.api;
+
+/**
+ * Created by Bianca on 12.10.2017.
+ */
+
+public interface RaiffeisenApiService {
+
+
+}
